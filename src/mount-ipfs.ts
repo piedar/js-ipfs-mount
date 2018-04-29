c82f9ffa71dbf5a519053e3458e3458caec875b5
Bennjamin Blast <bennjamin.blast@gmail.com>
2018-04-28 23:10:18 -0400

    implement open, opendir, readdir

--- src/mount-ipfs.ts
+++ src/mount-ipfs.ts
@@ -30,6 +30,13 @@ export class IpfsMountable implements Mountable {
 }
 
 
+function codeFromError(err: any): number {
+  return typeof err === "number" ? err :
+                err instanceof Error && err.message === "file does not exist" ? fuse.ENOENT :
+                err instanceof Error && err.message === "path must contain at least one component" ? fuse.EPERM :
+                -1;
+}
+
 
 class IpfsMount implements fuse.MountOptions {
   constructor(private readonly ipfs: typeof IpfsApi) {
@@ -40,8 +47,19 @@ class IpfsMount implements fuse.MountOptions {
     return reply(fuse.EROFS)
   }
 
+  readonly open = (path: string, flags: number, cb: (code: number, fd: number) => void) => {
+    debug("open " + path)
+    return cb(0, 22)
+  }
+
+  readonly opendir = (path: string, flags: number, cb: (code: number, fd: number) => void) => {
+    debug("opendir " + path)
+    if (path === "/") return cb(fuse.EPERM, -1)
+    return cb(0, 22)
+  }
+
   //readonly statfs = (path: string, cb: (code: number, fsStat: fuse.FSStat) => void) => {
-  //  console.log("go go statfs")
+  //  debug("statfs " + path)
   //}
 
   readonly getattr = (path: string, cb: (code: number, stats: fuse.Stats) => void) => {
@@ -50,11 +68,8 @@ class IpfsMount implements fuse.MountOptions {
       cb(code, stats)
     }
     const bail = (err: any, reason?: any) => {
-      const code = typeof err === "number" ? err :
-                   err instanceof Error && err.message === "file does not exist" ? fuse.ENOENT :
-                   -1;
       debug({ err, reason });
-      reply(code, undefined!)
+      reply(codeFromError(err), undefined!)
     }
 
     const now = new Date(Date.now())
@@ -69,28 +84,56 @@ class IpfsMount implements fuse.MountOptions {
       rdev: 0,
       blksize: 0,
       blocks: 0,
-
       mtime: now,
       atime: now,
       ctime: now,
     }
 
-    const ipfsPath = path === "/" ? path : "/ipfs/" + path
+    const ipfsPath = path === "/" ? path : "/ipfs/"+path
 
-    this.ipfs.files.stat(ipfsPath, (err: any, ipfsStat: any) => {
-      if (err) return bail(err, "ipfs files stat")
-
-      let [filetype, permissions] =
-        ipfsStat.type === "directory" ? [fs.constants.S_IFDIR, 0o111] :
-        ipfsStat.type === "file"      ? [fs.constants.S_IFREG, 0o444] :
-                                        [0, 0o000]
-
-      stats = Object.assign(stats, {
-        size: ipfsStat.size,
-        nlink: 1,
-        mode: filetype | permissions
+    this.ipfs.files.stat(ipfsPath)
+      .then((ipfsStat: any) => {
+        let [filetype, permissions] =
+          ipfsStat.type === "directory" ? [fs.constants.S_IFDIR, 0o111] :
+          ipfsStat.type === "file"      ? [fs.constants.S_IFREG, 0o444] :
+                                          [0, 0o000]
+        stats = Object.assign(stats, {
+          size: ipfsStat.size,
+          nlink: 1,
+          mode: filetype | permissions
+        })
+        return reply(0, stats as fuse.Stats)
       })
-      return reply(0, stats as fuse.Stats)
-    })
+      .catch((err: any) => bail(err, "ipfs files stat"))
   }
+
+  readonly readdir = (path: string, cb: (code: number, lst: string[]) => void) => {
+    debug("readdir " + path)
+
+    const reply = (code: number, files: string[]) => {
+      debug({ files });
+      cb(code, files)
+    }
+    const bail = (err: any, reason?: any) => {
+      debug({ err, reason });
+      reply(codeFromError(err), [])
+    }
+
+    // todo: extra slashes cause "Error: path must contain at least one component"
+    const ipfsPath = path === "/" ? path : "/ipfs"+path
+
+    this.ipfs.ls(ipfsPath)
+      .then((files: IpfsFileListing[]) => reply(0,
+        files.filter(file => file.depth === 1).map(file => file.name)))
+      .catch((err: any) => bail(err, "ipfs ls"))
+  }
+}
+
+type IpfsFileListing = {
+  depth: number,
+  name: string,
+  path: string,
+  size: number,
+  hash: string,
+  type: "file" | "directory",
 }
