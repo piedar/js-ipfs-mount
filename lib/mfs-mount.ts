77cae46fa3ab64896cc88e03e27ebe81347e5329
Bennjamin Blast <bennjamin.blast@gmail.com>
2018-06-24 19:31:51 -0400

    WIP: new writeback

--- lib/mfs-mount.ts
+++ lib/mfs-mount.ts
@@ -16,7 +16,7 @@ export class MfsMountable implements Mountable {
     fuseOptions: fuse.MountOptions = { },
   ) {
     const ipfs = new IpfsApi(ipfsOptions)
-    const writer = MfsWriter_Direct(ipfs)
+    const writer = MfsWriter_WriteBack(ipfs)
     // caller's options override the defaults
     this.fuseOptions = Object.assign(MfsMount(ipfs, writer), fuseOptions)
   }
@@ -130,5 +130,150 @@ function MfsWriter_Direct(ipfs: typeof IpfsApi): MfsWriter {
     flush: (path: string) => {
       return ipfs.files.flush(path)
     }
+  }
+}
+
+
+
+
+
+type Range = {
+  start: number, end: number,
+}
+
+function length(x: Range) {
+  return x.end + 1 - x.start
+}
+
+/// true iff y starts immediately after x ends
+function contiguous(x: Range, y: Range): boolean {
+  return x.end + 1 === y.start
+}
+
+/// true iff x and y do not overlap
+function discrete(x: Range, y: Range): boolean {
+  return x.end <= y.start || y.end <= x.start
+}
+
+function overlap(x: Range, y: Range) { return !discrete(x, y) }
+
+
+
+type WritableChunk = Range & {
+  path: string,
+  buffer: Buffer,
+}
+
+
+function merge(bottom: WritableChunk, top: WritableChunk) {
+  if (bottom.path !== top.path) return undefined
+  if (discrete(bottom, top) && !contiguous(bottom, top)) return undefined
+
+  const topStart = 0
+  const topEnd = topStart + length(top) - 1
+
+  const bottomStart = bottom.start < top.start ? 0 : topEnd + 1
+  const bottomEnd = bottomStart + length(bottom) - 1
+
+  const merged = {
+    start: Math.min(bottom.start, top.start),
+    end: Math.max(bottom.end, top.end),
+    path: bottom.path,
+    buffer: Buffer.concat([
+      // slice end is not inclusive!
+      bottom.buffer.slice(bottomStart, bottomEnd + 1),
+      top.buffer.slice(topStart, topEnd + 1)
+    ]),
+  }
+
+  if (length(merged) !== merged.buffer.byteLength) {
+    throw new Error(`expected length ${length(merged)} but got length ${merged.buffer.byteLength}`)
+  }
+
+  debug("merged chunks", { start: merged.start, end: merged.end, length: length(merged) })
+  return merged
+}
+
+function* mergeAll(existing: WritableChunk[], top: WritableChunk) {
+  let merged = undefined
+
+  for (const bottom of existing) {
+    if (!merged) {
+      merged = merge(bottom, top)
+      if (merged) {
+        yield merged
+        top = merged
+        continue
+      }
+    }
+
+    yield bottom
+  }
+
+  if (!merged) {
+    yield top
+  }
+}
+
+function* splitAll(existing: WritableChunk[], maxLength: number): IterableIterator<WritableChunk> {
+  for (const bottom of existing) {
+    if (length(bottom) <= maxLength) {
+      yield bottom
+      continue
+    }
+
+    yield bottom // todo
+    continue
+
+    /*
+    const bottomStart = 0
+    const bottomEnd = bottomStart + maxLength - 1
+
+    const newTopStart = bottomEnd + 1
+    const newTopEnd = length(bottom) - newTopStart
+
+    yield {
+      start: bottom.start,
+      end: bottom.start + maxLength - 1,
+      path: bottom.path,
+      buffer: bottom.buffer.slice(bottomStart, bottomEnd + 1)
+    }
+    */
+  }
+}
+
+function MfsWriter_WriteBack(ipfs: typeof IpfsApi): MfsWriter {
+  const targetLength = 1024 * 1024
+  let pendingChunks = new Array<WritableChunk>()
+
+  async function writeback(predicate: (c: WritableChunk) => boolean) {
+    for (let i = pendingChunks.length - 1; i >= 0; i--) {
+      const chunk = pendingChunks[i]
+      if (predicate(chunk)) {
+        debug("writeback", { start: chunk.start, end: chunk.end, length: length(chunk) })
+        pendingChunks.splice(i, 1)
+        await ipfs.files.write(chunk.path, chunk.buffer, {
+          offset: chunk.start, count: length(chunk)
+        })
+      }
+    }
+  }
+
+  return {
+    write: (path, buffer, offset, count) => {
+      pendingChunks = Array.from(mergeAll(pendingChunks, {
+        start: offset, end: offset + count - 1,
+        path: path, buffer: buffer,
+      }))
+
+      pendingChunks = Array.from(splitAll(pendingChunks, targetLength))
+
+
+      return writeback(c => length(c) >= targetLength)
+    },
+
+    flush: (path) => {
+      return writeback(c => true)
+    },
   }
 }
