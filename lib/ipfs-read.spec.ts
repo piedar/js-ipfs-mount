d779f0a0d5d33615dbf062ec04165b2c6c9c3471
Bennjamin Blast <bennjamin.blast@gmail.com>
2018-07-08 22:51:27 -0400

    refactor ipfs-read.spec

--- lib/ipfs-read.spec.ts
+++ lib/ipfs-read.spec.ts
@@ -1,25 +1,33 @@
+import * as fs from "fs"
 import { expect } from "chai"
-import * as mocha from "mocha"
+import { describe, it } from "mocha"
+import { promisify } from "util"
 import { IpfsReader, IpfsReader_Direct, IpfsReader_ReadStream } from "./ipfs-read"
 const IpfsApi = require("ipfs-api")
+const readFileAsync = promisify(fs.readFile)
 
 
 type TestCase = {
+  readonly name: string,
   readonly path: string,
-  readonly description: string,
-  readonly expectedBuffer: Readonly<Buffer>,
+  readonly expectedBuffer: Readonly<Buffer> | Promise<Readonly<Buffer>>,
 }
 
 const testCases: TestCase[] = [
   {
+    name: "empty",
     path: "/ipfs/QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH",
-    description: "empty",
     expectedBuffer: new Buffer(0),
   },
   {
+    name: "hello",
     path: "/ipfs/QmZULkCELmmk5XNfCgTnCyFgAVxBRBXyDHGGMVoLFLiXEN",
-    description: "hello",
     expectedBuffer: new Buffer("hello\n"),
+  },
+  {
+    name: "vlc-2.2.8.tar.xz",
+    path: "/ipfs/QmW6vbhabbRUHxfR98cnnDp1m5DjPCsvzZiowh4FbRZPAv",
+    expectedBuffer: readFileAsync("/usr/portage/distfiles/vlc-2.2.8.tar.xz"),
   },
 ]
 
@@ -33,16 +41,18 @@ const readers = [
 for (const { name, reader } of readers) {
   describe(name, () => {
     for (const testCase of testCases) {
-      it(testCase.description, async () => {
+      it(testCase.name, async () => {
+        const expectedBuffer = await testCase.expectedBuffer
+
         // use longer buffer to test for overflow
-        const buffer = new Buffer(testCase.expectedBuffer.byteLength + 4)
+        const buffer = new Buffer(expectedBuffer.byteLength + 4)
         const segment = { offset: 0, length: buffer.byteLength }
         const result = await reader.read(testCase.path, buffer, segment)
 
         expect(result.offset).to.deep.equal(segment.offset)
 
         const actual = buffer.slice(0, result.length)
-        expect(actual).to.deep.equal(testCase.expectedBuffer)
+        expect(actual).to.deep.equal(expectedBuffer)
 
         const leftover = buffer.slice(result.length)
         expect(leftover).to.deep.equal(new Buffer(leftover.byteLength), "overflow into empty bytes!")
