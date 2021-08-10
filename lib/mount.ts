6fe18cde38d6b2d492ce19de2c2badf154229029
Bennjamin Blast <bennjamin.blast@gmail.com>
2021-08-10 17:34:40 -0400

    chore: upgrade dependencies

--- lib/mount.ts
+++ lib/mount.ts
@@ -4,13 +4,13 @@ const debug = require("debug")("mount")
 
 export async function untilDone<TResult>(fuse: Fuse, done: () => Promise<TResult>): Promise<TResult> {
   debug("mount pending")
-  await new Promise((resolve, reject) => fuse.mount(err => err ? reject(err) : resolve()))
+  await new Promise<void>((resolve, reject) => fuse.mount(err => err ? reject(err) : resolve()))
   debug("mount ready")
 
   try { return await done() }
   finally {
     debug("unmount pending")
-    await new Promise((resolve, reject) => fuse.unmount(err => err ? reject(err) : resolve()))
+    await new Promise<void>((resolve, reject) => fuse.unmount(err => err ? reject(err) : resolve()))
     debug("unmount complete")
   }
 }
